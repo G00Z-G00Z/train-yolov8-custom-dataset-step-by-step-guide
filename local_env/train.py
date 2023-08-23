c82ce792fefe54c6559e42b4aaa1c9178b818dd2
G00Z_G00Z <edygomezgg4@gmail.com>
2023-08-23 12:56:56 -0600

    Updated train file to be better

--- local_env/train.py
+++ local_env/train.py
@@ -1,11 +1,15 @@
 from ultralytics import YOLO
 
+last_best_train_run_no: int = 27
+EPOCHS: int = 100
 # Load a model
-model = YOLO("yolov8n.yaml")  # build a new model from scratch
+model = YOLO(
+    f"../runs/detect/train{last_best_train_run_no}/weights/best.pt"
+)  # build a new model from scratch
 
 # Use the model
 results = model.train(
     data="config.yaml",
-    epochs=100,
+    epochs=EPOCHS,
     amp=False,
 )  # train the model
