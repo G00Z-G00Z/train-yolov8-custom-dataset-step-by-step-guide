d4532ac10bc564e9127d0385a55ad6f1752c1c6c
G00Z_G00Z <edygomezgg4@gmail.com>
2023-08-22 18:21:56 -0600

    Updated paths in yolo format

--- prepare_data/create_dataset_yolo_format.py
+++ prepare_data/create_dataset_yolo_format.py
@@ -2,37 +2,47 @@ import os
 import shutil
 
 
-DATA_ALL_DIR = os.path.join('.', 'data_all')
+DATA_ALL_DIR = os.path.join(".", "dataset")
 
-DATA_OUT_DIR = os.path.join('.', 'data')
+DATA_OUT_DIR = os.path.join(".", "yolo_format")
 
-for set_ in ['train', 'validation', 'test']:
-    for dir_ in [os.path.join(DATA_OUT_DIR, set_),
-                 os.path.join(DATA_OUT_DIR, set_, 'imgs'),
-                 os.path.join(DATA_OUT_DIR, set_, 'anns')]:
+for set_ in ["train", "validation", "test"]:
+    for dir_ in [
+        os.path.join(DATA_OUT_DIR, set_),
+        os.path.join(DATA_OUT_DIR, set_, "imgs"),
+        os.path.join(DATA_OUT_DIR, set_, "anns"),
+    ]:
         if os.path.exists(dir_):
             shutil.rmtree(dir_)
         os.mkdir(dir_)
 
-alpaca_id = '/m/0pcr'
+alpaca_id = "/m/0pcr"
 
-train_bboxes_filename = os.path.join('.', 'oidv6-train-annotations-bbox.csv')
-validation_bboxes_filename = os.path.join('.', 'validation-annotations-bbox.csv')
-test_bboxes_filename = os.path.join('.', 'test-annotations-bbox.csv')
+train_bboxes_filename = os.path.join(".", "oidv6-train-annotations-bbox.csv")
+validation_bboxes_filename = os.path.join(".", "validation-annotations-bbox.csv")
+test_bboxes_filename = os.path.join(".", "test-annotations-bbox.csv")
 
 
-for j, filename in enumerate([train_bboxes_filename, validation_bboxes_filename, test_bboxes_filename]):
-    set_ = ['train', 'validation', 'test'][j]
+for j, filename in enumerate(
+    [train_bboxes_filename, validation_bboxes_filename, test_bboxes_filename]
+):
+    set_ = ["train", "validation", "test"][j]
     print(filename)
-    with open(filename, 'r') as f:
+    with open(filename, "r") as f:
         line = f.readline()
         while len(line) != 0:
-            id, _, class_name, _, x1, x2, y1, y2, _, _, _, _, _ = line.split(',')[:13]
+            id, _, class_name, _, x1, x2, y1, y2, _, _, _, _, _ = line.split(",")[:13]
             if class_name in [alpaca_id]:
-                if not os.path.exists(os.path.join(DATA_OUT_DIR, set_, 'imgs', '{}.jpg'.format(id))):
-                    shutil.copy(os.path.join(DATA_ALL_DIR, '{}.jpg'.format(id)),
-                                os.path.join(DATA_OUT_DIR, set_, 'imgs', '{}.jpg'.format(id)))
-                with open(os.path.join(DATA_OUT_DIR, set_, 'anns', '{}.txt'.format(id)), 'a') as f_ann:
+                if not os.path.exists(
+                    os.path.join(DATA_OUT_DIR, set_, "imgs", "{}.jpg".format(id))
+                ):
+                    shutil.copy(
+                        os.path.join(DATA_ALL_DIR, "{}.jpg".format(id)),
+                        os.path.join(DATA_OUT_DIR, set_, "imgs", "{}.jpg".format(id)),
+                    )
+                with open(
+                    os.path.join(DATA_OUT_DIR, set_, "anns", "{}.txt".format(id)), "a"
+                ) as f_ann:
                     # class_id, xc, yx, w, h
                     x1, x2, y1, y2 = [float(j) for j in [x1, x2, y1, y2]]
                     xc = (x1 + x2) / 2
@@ -40,7 +50,7 @@ for j, filename in enumerate([train_bboxes_filename, validation_bboxes_filename,
                     w = x2 - x1
                     h = y2 - y1
 
-                    f_ann.write('0 {} {} {} {}\n'.format(xc, yc, w, h))
+                    f_ann.write("0 {} {} {} {}\n".format(xc, yc, w, h))
                     f_ann.close()
 
             line = f.readline()
