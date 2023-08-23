df1b3a231b2ed7ba86b1ba0851fc463233be1114
G00Z_G00Z <edygomezgg4@gmail.com>
2023-08-22 18:15:57 -0600

    Changed image_list_file for more general id

--- prepare_data/create_image_list_file.py
+++ prepare_data/create_image_list_file.py
@@ -1,25 +1,29 @@
 import os
 
 
-alpaca_id = '/m/0pcr'
+# This is the class id for the class in the repository
+alpaca_id = "/m/0pcr"
+class_id = alpaca_id
 
-train_bboxes_filename = os.path.join('.', 'oidv6-train-annotations-bbox.csv')
-validation_bboxes_filename = os.path.join('.', 'validation-annotations-bbox.csv')
-test_bboxes_filename = os.path.join('.', 'test-annotations-bbox.csv')
+train_bboxes_filename = os.path.join(".", "oidv6-train-annotations-bbox.csv")
+validation_bboxes_filename = os.path.join(".", "validation-annotations-bbox.csv")
+test_bboxes_filename = os.path.join(".", "test-annotations-bbox.csv")
 
-image_list_file_path = os.path.join('.', 'image_list_file')
+image_list_file_path = os.path.join(".", "image_list_file")
 
 image_list_file_list = []
-for j, filename in enumerate([train_bboxes_filename, validation_bboxes_filename, test_bboxes_filename]):
+for j, filename in enumerate(
+    [train_bboxes_filename, validation_bboxes_filename, test_bboxes_filename]
+):
     print(filename)
-    with open(filename, 'r') as f:
+    with open(filename, "r") as f:
         line = f.readline()
         while len(line) != 0:
-            id, _, class_name, _, x1, x2, y1, y2, _, _, _, _, _ = line.split(',')[:13]
-            if class_name in [alpaca_id] and id not in image_list_file_list:
+            id, _, class_name, _, x1, x2, y1, y2, _, _, _, _, _ = line.split(",")[:13]
+            if class_name in [class_id] and id not in image_list_file_list:
                 image_list_file_list.append(id)
-                with open(image_list_file_path, 'a') as fw:
-                    fw.write('{}/{}\n'.format(['train', 'validation', 'test'][j], id))
+                with open(image_list_file_path, "a") as fw:
+                    fw.write("{}/{}\n".format(["train", "validation", "test"][j], id))
             line = f.readline()
 
         f.close()
